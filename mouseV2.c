b8da1e929b6e5df64cc8548029cd8aa063a44688
Lucas Gabriel <142092072+LucaasGy@users.noreply.github.com>
2024-04-17 10:07:49 -0300

    fix: Correção clique do mouse

--- mouseV2.c
+++ mouseV2.c
@@ -1,63 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define mouseDireito "0400 0400 0200 0900"
-#define mouseEsquerdo "0400 0400 0100 0900"
-
 int main() {
     FILE *fp;
-
-    int cont1 = 0, cont2 = 0;
+    char buffer[80];
 
     while (1) {
         //Abre o comando xxd -E -l 144 /dev/input/event12 em modo de leitura
-        fp = popen("xxd -E -l 144 /dev/input/event0", "r");
+        fp = popen("xxd -E -l 96 /dev/input/event0", "r");
         if (fp == NULL) {
             printf("Erro ao abrir o comando.\n");
             return 1;
         }
 
-        char buffer[256];
-
         //enquanto não ler todas as linhas
         while (fgets(buffer, sizeof(buffer), fp) != NULL) {
             //caso strstr não retorne null ( tem o padrão )
             if (strstr(buffer, mouseDireito) != NULL) {
-                if(cont1==0){
-                    printf("Botão direito clicado!\n");
-                    cont1++;
-                }    
-                
-                else
-                    cont1=0;
-
+                printf("Botão direito clicado!\n");
+                memset(buffer, 0, sizeof(buffer));
                 break;
             }
 
             //caso strstr não retorne null ( tem o padrão )
             else if (strstr(buffer, mouseEsquerdo) != NULL) {
-                if(cont2==0){
-                    printf("Botão esquerdo clicado!\n");
-                    cont2++;
-                }    
-                
-                else
-                    cont2=0;
-
-                break;
+                printf("Botão esquerdo clicado!\n");
+                memset(buffer, 0, sizeof(buffer));     
+	            break;
             }
-            
-            //caso não seja uma linha desejada, limpa o buffer com a linha atual para não restar nenhum caractere para a proxima iteração ( nova linha )
-            //else
-                //memset(buffer, 0, sizeof(buffer));
-        }
 
+	        memset(buffer, 0, sizeof(buffer));
+        }
         pclose(fp);
     }
-
     return 0;
 }
-
-
-
