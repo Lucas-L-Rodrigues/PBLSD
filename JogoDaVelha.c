dbb35db80c6c8a967ec16fbf5e142ac4f5a04aa6
Lucas Gabriel <lucasfacul20222@gmail.com>
2024-04-30 20:36:50 -0300

    chore: Renomeando arquivos

---
+++ JogoDaVelha.c
@@ -0,0 +1,223 @@
+#include "utilsV4.h"
+
+int main() {
+    //Matriz do tabuleiro
+    char tabuleiro[3][3];
+    
+    //Jogador da vez e jogador vencedor
+    char jogador, vencedor;
+    
+    //Linha e coluna da matriz
+    int linha, coluna;
+    
+    //Número de jogadas e botões 1 ou 4 da placa
+    int jogadas, botoes;
+    
+    //Quadrante selecionado
+    int quadrante;
+
+    //Subida e descida do clique esquerdo do mouse e confirmação da jogada
+    int cliqueEsq, confirma;
+
+    //Temporizadores para contabilizar eventos necessários para mudar quadrante
+    int tempCima, tempBaixo, tempDireita, tempEsquerda;
+    
+    //Ponteiro para acesso ao arquivo de leitura do evento do mouse
+    FILE *fp;
+    
+    //Vetor que pega linha por linha de um evento do mouse
+    char buffer[70];
+    
+    //Limpa buffer
+    KEY_close();
+
+    //Verifica se botão está sendo lido
+    if (!KEY_open ( )){
+        printf("Erro na inicialização dos botoes.\n");
+        return -1;
+    }
+
+    system("clear");
+
+    //Menu inicial
+    exibirTela('m', ' ');
+    
+    //Lê botões da placa
+    KEY_read (&botoes);
+    while (botoes!=8 && botoes!=1)
+        KEY_read (&botoes);
+
+    //Botão 1 pressionado
+    if (botoes==8) {
+        cliqueEsq = 0;
+        tempCima = 0, tempBaixo = 0, tempDireita = 0, tempEsquerda = 0;
+	    do {
+            system("clear");
+            inicializarTabuleiro(&tabuleiro);
+            jogadas = 0;
+            jogador = 'X';
+            quadrante = 1;
+	    confirma = 0;
+
+            do {
+                imprimirQuadrante(&jogador,&quadrante,&tabuleiro);
+                confirma = 0;
+                memset(buffer, 0, sizeof(buffer));
+
+                //Capta eventos do mouse até botão esquerdo ser pressionado
+                do {
+                    //Abre o comando xxd -E -l 48 /dev/input/event0 em modo de leitura -> 48 pois so precisa ler 3 linhas
+                    fp = popen("xxd -E -l 48 /dev/input/event0", "r");
+                    if (fp == NULL) {
+                        printf("Erro ao abrir o comando.\n");
+                        return 1;
+                    }
+
+                    //Enquanto não ler todas as linhas do evento ( 3 linhas ao total )
+                    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+
+                        //Caso strstr não retorne null -> Clicou botão esquerdo do mouse
+                        if (strstr(buffer, mouseEsquerdo) != NULL) {
+                            //pressionei botão esquerdo do mouse
+                            if(cliqueEsq==0){
+                                confirma = 1;
+                                cliqueEsq++;
+                            } 
+
+                            //soltei botão esquerdo do mouse
+                            else
+                                cliqueEsq = 0;
+                            
+                            //limpa buffer ( apaga linha já lida )
+                            memset(buffer, 0, sizeof(buffer));
+                            break;
+                        }
+
+                        //Caso strstr não retorne null -> Moveu mouse para cima
+                        else if (strstr(buffer, mouseMovCima) != NULL) {
+                            if (quadrante > 3) {
+                                tempCima += 1;
+                                if (tempCima == sensibilidade){
+                                    quadrante -= 3;
+                                    zeraTemp(&tempBaixo,&tempCima,&tempDireita,&tempEsquerda);
+                                    system("clear");
+                                    imprimirQuadrante(&jogador,&quadrante,&tabuleiro);
+                                }
+                            }
+
+                            memset(buffer, 0, sizeof(buffer));
+                            break;
+                        }
+
+                        //Caso strstr não retorne null -> Moveu mouse para baixo
+                        else if (strstr(buffer, mouseMovBaixo) != NULL) {
+                            if (quadrante < 7) {
+                                tempBaixo += 1;
+                                if (tempBaixo == sensibilidade){
+                                    quadrante += 3;
+                                    zeraTemp(&tempBaixo,&tempCima,&tempDireita,&tempEsquerda);
+                                    system("clear");
+                                    imprimirQuadrante(&jogador,&quadrante,&tabuleiro);
+                                }
+                            }
+
+                            memset(buffer, 0, sizeof(buffer));
+                            break;
+                        }
+
+                        //Caso strstr não retorne null -> Moveu mouse para esquerda
+                        else if (strstr(buffer, mouseMovEsquerda) != NULL) {
+                            if ((quadrante != 1) && (quadrante != 4) && (quadrante != 7)) {
+                                tempEsquerda += 1;
+                                if (tempEsquerda == sensibilidade){
+                                    quadrante -= 1;
+                                    zeraTemp(&tempBaixo,&tempCima,&tempDireita,&tempEsquerda);
+                                    system("clear");
+                                    imprimirQuadrante(&jogador,&quadrante,&tabuleiro);
+                                }
+                            }
+
+                            memset(buffer, 0, sizeof(buffer));
+                            break;
+                        }
+
+                        //Caso strstr não retorne null -> Moveu mouse para direita
+                        else if (strstr(buffer, mouseMovDireita) != NULL) {
+                            if ((quadrante != 3) && (quadrante != 6) && (quadrante != 9)) {
+                                tempDireita += 1;
+                                if (tempDireita == sensibilidade){
+                                    quadrante += 1;
+                                    zeraTemp(&tempBaixo,&tempCima,&tempDireita,&tempEsquerda);
+                                    system("clear");
+                                    imprimirQuadrante(&jogador,&quadrante,&tabuleiro);
+                                }
+                            }
+
+                            memset(buffer, 0, sizeof(buffer));
+                            break;
+                        }
+
+                        //Limpa buffer ( apaga linha já lida )
+                        memset(buffer, 0, sizeof(buffer));
+                    }
+                    
+                    //Fecha arquivo de captura dos eventos do mouse
+                    pclose(fp);
+                
+                //Enquanto não for clicado botão esquerdo do mouse
+                } while (confirma==0);
+
+                //Relaciona o contador ( quadrante selecionado ) com linha e coluna do tabuleiro
+                linha = qualLinha(quadrante);
+                coluna = qualColuna(quadrante);
+                
+                //Verifica se a posição está vazia
+                if (tabuleiro[linha][coluna] == ' ') {
+                    tabuleiro[linha][coluna] = jogador;
+                    jogadas++;
+                    quadrante = 1;
+
+                    //Verifica se já tem um vencedor
+                    vencedor = verificarVencedor(&tabuleiro);
+
+                    //Troca de jogador se não houver vencedor
+                    if(vencedor==' ')
+                        jogador = (jogador == 'X') ? 'O' : 'X';
+                    
+                    system("clear");
+                }
+                
+                //Posição já está marcada
+                else{
+                    system("clear");
+                    printf(vermelho"\t\tPosição ocupada. Tente novamente."padrao"\n\n");
+                }
+            //Enquanto não tiver um vencedor ou número de jogadas for menor que 9
+            } while (vencedor==' ' && jogadas < 9);
+
+            //Tabuleiro final
+            imprimirTabuleiro(&tabuleiro,0);
+
+            //Se alguém venceu, exibe menu do vencedor
+            if (vencedor != ' ') 
+                exibirTela('v', vencedor);
+            
+            //Se empatou, exibe menu de empate
+            else
+                exibirTela('e', ' ');
+    
+            //Lê botões
+            KEY_read (&botoes);
+            while (botoes!=8 && botoes!=1)
+                KEY_read (&botoes);
+        //Enquanto primeiro botão for pressionado
+        } while (botoes==8);
+    }
+    
+    printf("\nSaindo. Até a próxima!\n\n");
+
+    //Fecha conexão com os botões da placa
+    KEY_close ( );
+
+    return 0;
+}
---
+++ utilsCliquesMouse.h
@@ -0,0 +1,147 @@
+#ifndef UTILSV3_H
+#define UTILSV3_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <intelfpgaup/KEY.h>
+
+//Padrões dos eventos do clique dos botões direito e esquerdo do mouse
+#define mouseDireito "0400 0200 0900"
+#define mouseEsquerdo "0400 0100 0900"
+
+#define padrao "\033[0m"
+#define vermelho "\033[1;31m"
+#define verde "\033[1;32m"
+#define azulClaro "\033[1;36m" 
+
+//Função para relacionar valor do contador com a linha do tabuleiro (primeiro termo da matriz)
+int qualLinha(int cont) {
+    int linha;
+    
+    if (cont <= 3)
+        linha = 0;
+
+    else if (cont <= 6)
+        linha = 1;
+    
+    else 
+        linha = 2;
+
+    return linha;
+}
+
+//Função para relacionar valor do contador com a coluna do tabuleiro (segundo termo da matriz)
+int qualColuna(int cont) {
+    int coluna;
+    
+    if (cont == 1 || cont == 4 || cont == 7)
+        coluna = 0;
+
+    else if (cont == 2 || cont == 5 || cont == 8)
+        coluna = 1;
+    
+    else
+        coluna = 2;
+
+    return coluna;
+}
+
+//Função para inicializar o tabuleiro com espaços em branco
+void inicializarTabuleiro(char (*tabuleiro)[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            (*tabuleiro)[i][j] = ' ';
+        }
+    }
+}
+
+//Função para imprimir o tabuleiro
+void imprimirTabuleiro(char (*tabuleiro)[3][3], int matrizEspelho) {
+    printf("\n");
+
+    for (int i = 0; i < 3; i++) {
+        printf("\t\t %c │ %c │ %c", (*tabuleiro)[i][0], (*tabuleiro)[i][1], (*tabuleiro)[i][2]);
+    
+        if (matrizEspelho)
+            printf("\t\t %d │ %d │ %d", (i*3)+1, (i*3)+2, (i*3)+3);
+
+        printf("\n");
+
+        if (i < 2) {
+            printf("\t\t───┼───┼───");
+            
+            if (matrizEspelho)
+                printf("\t\t───┼───┼───");
+
+            printf("\n");
+        }
+    }
+
+    printf("\n");
+}
+
+//Função para imprimir o quadrante selecionado
+void imprimirQuadrante(char *jogador, int *quadrante, char (*tabuleiro)[3][3]) {
+    printf(azulClaro"\t\tJogador %c - Quadrante selecionado: %d"padrao"\n\n",*jogador, *quadrante);
+    imprimirTabuleiro(tabuleiro,1);
+    printf(azulClaro"\nPressione botão direito do mouse para selecionar quadrante"padrao"\n");
+}
+
+//Função para verificar se há um vencedor e retornar simbolo ( X ou O )
+char verificarVencedor(char (*tabuleiro)[3][3]) {
+    //Verificar linhas e colunas
+    for (int i = 0; i < 3; i++) {
+        if ((*tabuleiro)[i][0] == (*tabuleiro)[i][1] && (*tabuleiro)[i][0] == (*tabuleiro)[i][2])
+            return (*tabuleiro)[i][0];
+        if ((*tabuleiro)[0][i] == (*tabuleiro)[1][i] && (*tabuleiro)[0][i] == (*tabuleiro)[2][i])
+            return (*tabuleiro)[0][i];
+    }
+
+    //Verificar diagonais
+    if ((*tabuleiro)[0][0] == (*tabuleiro)[1][1] && (*tabuleiro)[0][0] == (*tabuleiro)[2][2])
+        return (*tabuleiro)[0][0];
+    
+    if ((*tabuleiro)[0][2] == (*tabuleiro)[1][1] && (*tabuleiro)[0][2] == (*tabuleiro)[2][0])
+        return (*tabuleiro)[0][2];
+
+    //Se não houver vencedor, retorna vazio
+    return ' ';
+}
+
+//Função para exibição de menus
+void exibirTela(char tipo, char vencedor) {
+   
+    if(tipo == 'm')
+	    printf("\t");
+   
+    printf(" _________________________________________________ \n");
+    
+    //inicio
+    if (tipo == 'm') {
+        printf("\t|           "azulClaro"Bem-vindo ao Jogo da Velha!           "padrao"|\n");
+        printf("\t| ----------------------------------------------- |\n");
+        printf("\t| "azulClaro"[Botao 1] Iniciar jogo                          "padrao"|\n");
+        printf("\t| "azulClaro"[Botao 4] Sair                                  "padrao"|\n");
+        printf("\t");
+     } 
+
+    //vitória
+    else if (tipo == 'v') {
+        printf("|              "verde"O jogador %c venceu!"padrao"                |\n", vencedor);
+        printf("| ----------------------------------------------- |\n");
+        printf("| "azulClaro"[Botao 1] Jogar novamente                       "padrao"|\n");
+        printf("| "azulClaro"[Botao 4] Sair                                  "padrao"|\n");
+    } 
+
+    //empate
+    else if (tipo == 'e') {
+        printf("|                 "vermelho"O jogo empatou."padrao"                 |\n");
+        printf("| ----------------------------------------------- |\n");
+        printf("| "azulClaro"[Botao 1] Jogar novamente                       "padrao"|\n");
+        printf("| "azulClaro"[Botao 4] Sair                                  "padrao"|\n");
+    }
+    
+    printf("|_________________________________________________|"padrao"\n\n");
+}
+
+#endif
