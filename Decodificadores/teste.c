fe76f8f7c91493ebd4c4c27169dd9b0463193624
Lucas Gabriel <lucasfacul20222@gmail.com>
2024-05-06 00:40:09 -0300

    feat: Decodificador para pegar imagem dos padrões

---
+++ Decodificadores/teste.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    FILE *fp;
+    char buffer[70];
+
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        // Abre o comando xxd -E -l 48 /dev/input/event12 em modo de leitura
+        fp = popen("xxd -E -l 48 /dev/input/event0", "r");
+        if (fp == NULL) {
+            printf("Erro ao abrir o comando.\n");
+            return 1;
+        }
+
+        // Lê e imprime a saída do comando
+        while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+            printf("%s\n",buffer);
+            memset(buffer, 0, sizeof(buffer));
+        }
+        
+        printf("\n");
+        pclose(fp);
+    }
+
+    return 0;
+}
