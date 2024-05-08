b46884db36088db161bacbcba3b0083020605fed
Lucas Gabriel <lucasfacul20222@gmail.com>
2024-05-07 22:34:19 -0300

    feat: Atualizando teste

--- Decodificadores/teste.c
+++ Decodificadores/teste.c
@@ -4,45 +4,26 @@
 
 int main() {
     FILE *fp;
-    char buffer[70];
+    char buffer[300];
 
     while (1) {
         memset(buffer, 0, sizeof(buffer));
         // Abre o comando xxd -E -l 48 /dev/input/event12 em modo de leitura
-        fp = popen("xxd -E -l 48 /dev/input/event0", "r");
+        fp = popen("xxd -E -l 16 -p /dev/input/event0", "r");
         if (fp == NULL) {
             printf("Erro ao abrir o comando.\n");
             return 1;
         }
 
         // Lê e imprime a saída do comando
-        while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-            printf("%s\n",buffer);
+        if(fgets(buffer, sizeof(buffer), fp) != NULL) {
+            printf("%s",buffer);
+            printf("\n%d\n",strlen(buffer));
             memset(buffer, 0, sizeof(buffer));
         }
         
-        printf("\n");
         pclose(fp);
     }
-
-    /*while (1) {
-        memset(buffer, 0, sizeof(buffer));
-        // Abre o comando xxd -E -l 48 /dev/input/event12 em modo de leitura
-        fp = popen("xxd -E -l 48 -p /dev/input/event0", "r");
-        if (fp == NULL) {
-            printf("Erro ao abrir o comando.\n");
-            return 1;
-        }
-
-        // Lê e imprime a saída do comando
-        while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-            printf("%s\n",buffer);
-            memset(buffer, 0, sizeof(buffer));
-        }
-        
-        printf("\n");
-        pclose(fp);
-    }*/
 
     return 0;
 }
