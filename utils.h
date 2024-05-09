a98f18b8e92d11e3f6c08ce8f4e0b2ab1930dc8d
Lucas Gabriel <142092072+LucaasGy@users.noreply.github.com>
2024-05-09 12:07:00 -0300

    feat: Removendo -E do comando de abertura do arquivo especial

--- utils.h
+++ utils.h
@@ -15,11 +15,10 @@
 #define mouseMovEsquerda "02000000ffff"  //0200 0000 ffff
 
 //Comando para acessar arquivo especial de leitura dos eventos do mouse em hexadecimal
-//-E -> xxd deve usar a saída de texto, mesmo se o arquivo de entrada for um arquivo binário (ler como little-endian)
 //-l 14 -> limitar a saída a 14 bytes (dados suficientes para identificar padrões)
 //-p -> representação ASCII é desconsiderada
 //dev/input/event0 -> arquivo especial que grava eventos do mouse
-#define comandoArquivoMouse "xxd -E -l 14 -p /dev/input/event0"
+#define comandoArquivoMouse "xxd -l 14 -p /dev/input/event0"
 
 //Códigos para alteração de cores dos prints no terminal
 #define padrao "\033[0m"
